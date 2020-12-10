67f998492db61ea7bf81cd4300b8be3e2cb7ec55
9ui <71952965+9ui@users.noreply.github.com>
2020-12-10 15:32:59 +0800

    feat(init): init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script lang = "ts">
+import {  defineComponent } from "vue";
+export default defineComponent({
+  name: 'App',
+  setup(){}
+})
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,1 @@
+import type { UserConfig } from 'vite';
