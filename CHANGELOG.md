946993e55d2eb9a53b52ff3aefc51225a75b8557
9ui <71952965+9ui@users.noreply.github.com>
2020-12-10 17:17:31 +0800

    build(构建): 完善构建配置

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.0.1 (2020-12-10)
+
+### Features
+
+- **init:** init project ([67f9984](https://github.com/9ui/laihua-web6.0/commit/67f9984))
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,15 +5,15 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
-}
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+    data() {
+      return {
+        count: 0,
+      };
+    },
+  };
 </script>
