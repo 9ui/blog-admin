0812dcd362c19d2c2abbae04da0899b2a3ef2286
9ui <71952965+9ui@users.noreply.github.com>
2020-12-18 14:22:27 +0800

    chore: 修改vscode的配置文件

--- src/setup/ant-design-vue/index.ts
+++ src/setup/ant-design-vue/index.ts
@@ -6,6 +6,5 @@ import { Button } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
 export function setupAntd(app: App<Element>) {
-  // Here are the components required before registering and logging in
   app.use(Button);
 }
