5dea587a2baca5425cd76a570847da8d5f0a2c8e
9ui <71952965+9ui@users.noreply.github.com>
2020-12-19 11:14:06 +0800

    test: 测试

--- .github/actions/debug-action/__tests__/debug.test.ts
+++ .github/actions/debug-action/__tests__/debug.test.ts
@@ -1,6 +1,15 @@
 import * as core from '@actions/core';
 import run from '../debug';
 
+beforeEach(() => {
+  jest.resetModules();
+  process.env['INPUT_AMAZING-CREATURE'] = 'person';
+});
+
+afterEach(() => {
+  delete process.env['INPUT_AMAZING-CREATURE'];
+});
+
 describe('debug action debug messages', () => {
   it('outputs a debug message', async () => {
     const debugMock = jest.spyOn(core, 'debug');
