e380be769499af4fdfcf35f81febf68000651adf
zzw <zzw9059190@gmail.com>
2021-03-19 14:26:22 +0800

    fix: 修复文件引入错误

--- build/vite/plugin/html.ts
+++ build/vite/plugin/html.ts
@@ -7,17 +7,17 @@ import type { ViteEnv } from '../../utils';
 
 import html from 'vite-plugin-html';
 
-import pkg from '../../../package.json';
-import { GLOB_CONFIG_FILE_NAME } from '../../constant';
+// import pkg from '../../../package.json';
+// import { GLOB_CONFIG_FILE_NAME } from '../../constant';
 
 export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
-  const { VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH } = env;
+  const { VITE_GLOB_APP_TITLE } = env;
 
-  const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
+  //   const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
 
-  const getAppConfigSrc = () => {
-    return `${path || '/'}${GLOB_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
-  };
+  //   const getAppConfigSrc = () => {
+  //     return `${path || '/'}${GLOB_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
+  //   };
 
   const htmlPlugin: Plugin[] = html({
     minify: isBuild,
@@ -27,16 +27,7 @@ export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
         title: VITE_GLOB_APP_TITLE,
       },
       // Embed the generated app.config.js file
-      tags: isBuild
-        ? [
-            {
-              tag: 'script',
-              attrs: {
-                src: getAppConfigSrc(),
-              },
-            },
-          ]
-        : [],
+      tags: [],
     },
   });
   return htmlPlugin;
